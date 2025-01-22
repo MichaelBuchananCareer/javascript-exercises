05095b69d5f97ace850b16d67585c5cda35316f1
Michael Buchanan <Michael.Buchanan.Career@Gmail.com>
2025-01-22 14:21:28 -0500

    Fixed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let output = '';
+    for (let i=0; i < times; i++){
+        output += word;
+    }
+    return output;
 };
 
+repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
