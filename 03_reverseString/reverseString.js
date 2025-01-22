ea5cd6b3baabecd39fe819ed098223a60e142b89
Michael Buchanan <Michael.Buchanan.Career@Gmail.com>
2025-01-22 14:29:57 -0500

    Fixed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversed = "";
+    for (let i=word.length -1; i >= 0; i--){
+        reversed += word[i];
+    }
+    return reversed;
 
 };
+
+reverseString('Hello');
 
 // Do not edit below this line
 module.exports = reverseString;
