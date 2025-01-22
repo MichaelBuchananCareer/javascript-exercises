4e8afca9a7de24d53ac3be276a3a04fb79f90f37
Michael Buchanan <Michael.Buchanan.Career@Gmail.com>
2025-01-22 15:54:32 -0500

    Fixed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+	return value1 + value2;
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+    return value1 - value2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	output = 0;
+  array.forEach((item) => {
+    output += item;
+  })
+
+  return Number(output);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	output = 1;
+  array.forEach((item) => {
+    output *= item;
+  })
 
+  return Number(output);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	let output = 1;
+
+  for (let i=1; i <=value; i++){
+    output *= i;
+  }
+  return output;
+
 };
 
 // Do not edit below this line
