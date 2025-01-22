b5c552daa44460ffd20cbebe34d211a2ab147a4f
Michael Buchanan <Michael.Buchanan.Career@Gmail.com>
2025-01-22 15:02:12 -0500

    Fixed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    
+    if (!Number.isInteger(first) || !Number.isInteger(second)) { return 'ERROR'}
+    if (first < 0 || second < 0) {return "ERROR"}
 
-};
+    if (first > second){
+        for (let i=second; i <= first; i++ ){
+            sum += i;
+        }
+    }
+    if (first < second){
+        for (let i=first; i <= second; i++ ){
+            sum += i;
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
