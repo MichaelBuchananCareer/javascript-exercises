817a16c0e97f5e97f8f349f8f48e071104190e5c
Michael Buchanan <Michael.Buchanan.Career@Gmail.com>
2025-01-22 16:19:09 -0500

    Fixed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let letters = [];
+    let reverse = [];
 
+    for(let i=0; i < input.length; i++){
+        if (input[i].toUpperCase() != input[i].toLowerCase() || Number.parseInt(input[i])){
+            letters.push(input[i].toLowerCase());
+        }
+    }
+
+    for(let i=input.length-1; i >= 0; i--){
+        if (input[i].toUpperCase() != input[i].toLowerCase() || Number.parseInt(input[i])){
+            reverse.push(input[i].toLowerCase());
+        }
+    }
+
+    if (JSON.stringify(letters) === JSON.stringify(reverse)){
+        return true
+    } else {
+        return false
+    }
 };
+
+palindromes('r3ace3car');
 
 // Do not edit below this line
 module.exports = palindromes;
