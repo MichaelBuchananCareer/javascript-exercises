fb527fe3967286778b2874b0b47838aa3485a27d
Michael Buchanan <Michael.Buchanan.Career@Gmail.com>
2025-01-22 15:27:19 -0500

    Fixed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (((temp - 32)*(5/9)) === 0){
+    return 0
+  } else {
+    return Number(((temp - 32)*(5/9)).toFixed(1));}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Number(((temp * (9/5) + 32)).toFixed(1));
 };
 
 // Do not edit below this line
